4c11b6f3a1868b2e18f0391cdddbe0a8157833ca
BrettFerwerda <bferwerd@samford.edu>
2026-02-20 20:36:46 -0600

    Update DLinkedListQueue.java

--- chapter9/DLinkedListQueue.java
+++ chapter9/DLinkedListQueue.java
@@ -17,13 +17,11 @@ public class DLinkedListQueue<T> implements Queue<T> {
 
     @Override
     public T dequeue() throws Exception {
-        // TODO (dequeue from head using removeFirst())
         return list.removeFirst().getData();
     }
 
     @Override
     public T front() throws Exception {
-        // TODO (peek at head)
         return list.get(0).getData();
     }
 
