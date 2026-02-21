cfc1c42b58d06532207d1feaed45956ce8304809
BrettFerwerda <bferwerd@samford.edu>
2026-02-20 20:35:18 -0600

    Update SortedDLinkedListPriorityQueue.java

--- chapter9/SortedDLinkedListPriorityQueue.java
+++ chapter9/SortedDLinkedListPriorityQueue.java
@@ -8,6 +8,7 @@ public class SortedDLinkedListPriorityQueue<T> implements PriorityQueue<T> {
     private static class Entry<T> implements Comparable<Entry<T>> {
         final int priority;
         final T data;
+
         Entry(int priority, T data) {
             this.priority = priority;
             this.data = data;
@@ -32,35 +33,26 @@ public class SortedDLinkedListPriorityQueue<T> implements PriorityQueue<T> {
             list.add(newentry);
             return;
         }
-        
-        // Algorithm: make sure first item in DLInkedList is always highest priority
-        // Start with first item and traverse the list until you get to a higher priority item
-        // Insert the new item before the one you just found
+
         DNode<Entry<T>> curr = list.get(0);
-        while (curr!=null) {
-            if (priority<curr.getData().priority) {
+        while (curr != null) {
+            if (priority < curr.getData().priority) {
                 list.addBefore(curr, newentry);
                 return;
             }
             curr = curr.getNext();
         }
 
-        // if you get to the end of the list, add it at the end
-        if (curr==null) {
-            list.add(newentry); 
-        }
-
+        list.add(newentry);
     }
 
     @Override
     public T dequeue() throws Exception {
-        // TODO: removeFirst()
         return list.removeFirst().getData().data;
     }
 
     @Override
     public T front() throws Exception {
-        // TODO: peek head
         return list.get(0).getData().data;
     }
 
